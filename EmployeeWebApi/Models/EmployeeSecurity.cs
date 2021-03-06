b4b3c15ee551a8d94aae097ae464f590880959ba
Arunav <arunav@vonome.com>
2021-03-06 11:44:27 +0600

    Basic authentication filter is implemented

---
+++ EmployeeWebApi/Models/EmployeeSecurity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeWebApi.Models
+{
+    public class EmployeeSecurity
+    {
+        public static ApplicationDbContext _db;
+
+        public EmployeeSecurity(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Basic authentication is implemented
+        public static bool Login(string username, string password)
+        {
+            return _db.Users.Any(m =>
+                m.UserName.Equals(username, StringComparison.OrdinalIgnoreCase) && m.Password == password);
+        }
+    }
+}
